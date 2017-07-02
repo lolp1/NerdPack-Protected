e2bb0b1adf1651f1baad029de089b200b05dd534
Jaime Marques <mrthesoulz@gmail.com>
2017-07-02 17:25:26 +0100

    ishackenabled condition

--- conditions.lua
+++ conditions.lua
@@ -1,3 +1,12 @@
+local NeP = NeP
+local noop = function() return false end
+local _advanced = IsHackEnabled ~= nil or false
+local IsHackEnabled = IsHackEnabled or noop
+
 NeP.DSL:Register("advanced", function()
-	return not not IsHackEnabled
+	return _advanced
+end)
+
+NeP.DSL:Register("ishackenabled", function(_, hack)
+	return IsHackEnabled(hack)
 end)
