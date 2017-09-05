b387701730c00406476d5c618440bc2ce55bb4d8
Jaime Marques <mrthesoulz@gmail.com>
2017-09-05 18:35:12 +0100

    temp disable game objects OM for wtoolkit

--- wToolkit64.lua
+++ wToolkit64.lua
@@ -5,11 +5,18 @@ gbl.wToolkit64 = {}
 function gbl.wToolkit64.Load()
 end
 
+function gbl.wToolkit64_OM()
+	for i=1, _G.GetObjectCount() do
+		local Obj = _G.GetObjectWithIndex(i)
+		_G.NeP.OM:Add(Obj, false)
+	end
+end
+
 gbl:AddUnlocker('wToolkit64', {
-	test = function() return wToolkit64 end,
+	test = function() return _G.wToolkit64 end,
 	init = gbl.wToolkit64.Load,
 	prio = 2,
 	functions = gbl.Generic,
 	extended = gbl.FireHack,
-	om = gbl.FireHack_OM
+	om = gbl.wToolkit64_OM
 })
