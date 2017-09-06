07bb2e3d7cb8351a4fe04bc573906bb12944cde9
Jaime Marques <mrthesoulz@gmail.com>
2017-09-06 19:10:08 +0100

    re-enable GO for wtoolkit64

--- wToolkit64.lua
+++ wToolkit64.lua
@@ -5,18 +5,11 @@ gbl.wToolkit64 = {}
 function gbl.wToolkit64.Load()
 end
 
-function gbl.wToolkit64_OM()
-	for i=1, _G.GetObjectCount() do
-		local Obj = _G.GetObjectWithIndex(i)
-		_G.NeP.OM:Add(Obj, false)
-	end
-end
-
 gbl:AddUnlocker('wToolkit64', {
 	test = function() return _G.wToolkit64 end,
 	init = gbl.wToolkit64.Load,
 	prio = 2,
 	functions = gbl.Generic,
 	extended = gbl.FireHack,
-	om = gbl.wToolkit64_OM
+	om = gbl.FireHack_OM
 })
