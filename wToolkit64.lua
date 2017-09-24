2d8e0481b057face15a065c76e66bfa61f6f6dc9
Jaime Marques <mrthesoulz@gmail.com>
2017-09-24 17:48:52 +0100

    Update wToolkit64.lua

--- wToolkit64.lua
+++ wToolkit64.lua
@@ -1,8 +1,16 @@
 local _, gbl = ...
 
-gbl.wToolkit64 = {}
+gbl.wToolkit64 = setmetatable(gbl.FireHack, {})
 
 function gbl.wToolkit64.Load()
+end
+
+function gbl.wToolkit64.ObjectCreator(a)
+	return _G.ObjectIsVisible(a) and _G.ObjectCreator(a)
+end
+
+function gbl.wToolkit64.GameObjectIsAnimating(a)
+	return _G.ObjectIsVisible(a) and _G.GameObjectIsAnimating(a)
 end
 
 gbl:AddUnlocker('wToolkit64', {
@@ -10,6 +18,6 @@ gbl:AddUnlocker('wToolkit64', {
 	init = gbl.wToolkit64.Load,
 	prio = 2,
 	functions = gbl.Generic,
-	extended = gbl.FireHack,
+	extended = gbl.wToolkit64,
 	om = gbl.FireHack_OM
 })
