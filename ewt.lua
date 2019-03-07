a4b8afe0b72fea5db3068736db626bdb9ba2d823
Jaime Marques <mrthesoulz@gmail.com>
2019-03-07 19:31:59 +0000

    Update ewt.lua

--- ewt.lua
+++ ewt.lua
@@ -60,7 +60,7 @@ function f.LineOfSight(a, b)
         return true
     end
     local ax, ay, az = g.ObjectPosition(a)
-    if az == nil then return e end
+    if az == nil then return false end
     local bx, by, bz = g.ObjectPosition(b)
     if bz == nil then return false end
     return not g.TraceLine(ax, ay, az+2.25, bx, by, bz+2.25, g.bit.bor(0x10, 0x100))
