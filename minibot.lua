8e99e76f1c765da3a3f4cdd6eb31a1d2a19f0cca
jaime <mrthesoulz@gmail.com>
2020-12-01 19:39:07 +0000

    make minibot advanced

--- minibot.lua
+++ minibot.lua
@@ -1,14 +1,53 @@
 local _, gbl = ...
 local NeP = _G.NeP
-gbl.FireHack = gbl.MergeTable(gbl.Generic, {})
-local f = gbl.FireHack
+gbl.Minibot = gbl.MergeTable(gbl.EWT, {})
+local f = gbl.Minibot
 local g = gbl.gapis
+
+local ObjectTypes
+
+local wrappers = {
+	ObjectIsUnit = function(obj) return obj and g.ObjectIsType(obj, ObjectTypes.Unit) end,
+	ObjectIsPlayer = function(obj) return obj and g.ObjectIsType(obj, ObjectTypes.Player) end,
+	ObjectIsGameObject = function(obj) return obj and g.ObjectIsType(obj, ObjectTypes.GameObject) end,
+	ObjectIsAreaTrigger = function(obj) return obj and g.ObjectIsType(obj, ObjectTypes.AreaTrigger) end,
+}
+
+function f.Load()
+	local GetObjectTypeFlagsTable = _G.wmbapi.GetObjectTypeFlagsTable()
+	ObjectTypes = {
+		Object = GetObjectTypeFlagsTable.Object,
+		Item = GetObjectTypeFlagsTable.Item,
+		Container = GetObjectTypeFlagsTable.Container,
+		AzeriteEmpoweredItem = GetObjectTypeFlagsTable.AzeriteEmpoweredItem,
+		AzeriteItem = GetObjectTypeFlagsTable.AzeriteItem,
+		Unit = GetObjectTypeFlagsTable.Unit,
+		Player = GetObjectTypeFlagsTable.Player,
+		ActivePlayer = GetObjectTypeFlagsTable.ActivePlayer,
+		GameObject = GetObjectTypeFlagsTable.GameObject,
+		DynamicObject = GetObjectTypeFlagsTable.DynamicObject,
+		Corpse = GetObjectTypeFlagsTable.Corpse,
+		AreaTrigger = GetObjectTypeFlagsTable.AreaTrigger,
+		SceneObject = GetObjectTypeFlagsTable.SceneObject,
+		ConversationData = GetObjectTypeFlagsTable.Conversation
+	}
+	local gapis = gbl.MergeTable(_G.wmbapi, gbl.gapis)
+	gapis = gbl.MergeTable(wrappers, gapis)
+	gbl.gapis = gapis 
+end
+
+function f.OM_Maker()
+	for i=1, g.GetObjectCount() do
+		local Obj = g.GetObjectWithIndex(i)
+		NeP.OM:Add(Obj, g.ObjectIsType(Obj, ObjectTypes.GameObject))
+	end
+end
 
 gbl:AddUnlocker('Minibot', {
 	test = function()
-		return wmbapi ~= nil
+		return _G.wmbapi ~= nil
 	end,
     init = f.Load,
-	prio = 1,
+	prio = 4,
 	functions = f,
 })
