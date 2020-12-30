ec0eae49f1ce693bc6f9390ca38c588cf01674dc
jaime <mrthesoulz@gmail.com>
2020-12-30 06:25:09 +0000

    test

--- luabox.lua
+++ luabox.lua
@@ -6,6 +6,7 @@ local g = gbl.gapis
 local lb
 
 function f.Load()
+    NeP.Core:Print('LB is still under dev... v1')
     lb = g. _G.__LB__;
     local _G = _G
 	g.CameraOrSelectOrMoveStart = function (...) return lb.Unlock(_G.CameraOrSelectOrMoveStart, ...) end
