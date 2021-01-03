169aaf1624ccaba1847c3c3c5aec9d3ec911786b
Jaime Marques <mrthesoulz@gmail.com>
2021-01-03 03:25:55 +0000

    Update WowAdvanced.lua

--- WowAdvanced.lua
+++ WowAdvanced.lua
@@ -180,7 +180,7 @@ function f.LineOfSight(a, b)
 
     if ax and ay and az and bx and by and bz then
         local flags = bit.bor(0x10, 0x100, 0x1)
-        return (g.TraceLine(ax, ay, az, bx, by, bz, flags) == 0)
+        return (g.TraceLine(ax, ay, az, bx, by, bz, flags) == 1)
     end
     return false
 end
