73906c5d20c936e08c605f2277cb9253c0cf5bc4
jaime <mrthesoulz@gmail.com>
2021-01-15 23:21:52 +0000

    Update WowAdvanced.lua

--- WowAdvanced.lua
+++ WowAdvanced.lua
@@ -22,6 +22,9 @@ local Offsets = {
     ['RotationR'] = 0x650 + 0x10, --Location + 1
 }
 
+local cachedUnitResults = {}
+
+
 local function handleUnits(...)
     local mouseover;
     local focus;
@@ -43,7 +46,12 @@ end
 
 local function UnitTagHandler(func)
     return function(...)
-        return func(handleUnits(...))
+        local k1, k2, k3 = ...
+        local key = tostring(func) .. tostring(k1 or '') .. tostring(k2 or '') .. tostring(k3 or '')
+        if not cachedUnitResults[key] then
+            cachedUnitResults[key] = func(handleUnits(...))
+        end
+        return cachedUnitResults[key]
     end
 end
 
@@ -59,7 +67,8 @@ end
 
 function f.Load()
 
-    print('LOADED test v5')
+    print('LOADED test v6')
+    NeP.Timer.Add('nep_wa_cachedUnitResults', function() wipe(cachedUnitResults) end, 1)
 
     -- ADD GUID
     g.UnitInRange = UnitTagHandler(_G.UnitInRange)
